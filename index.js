6421f0183a221d50d50a0984bf8b15388302d383
lashatatu <lashatatu@gmail.com>
2020-10-05 17:59:31 +0400

    1

--- index.js
+++ index.js
@@ -1,18 +1,15 @@
-class Timer {
-    constructor(durationInput, startButton, pauseButton) {
-        this.durationInput = durationInput;
-        this.startButton = startButton;
-        this.pauseButton = pauseButton;
+const durationInput = document.querySelector(`#duration`);
+const startButton = document.querySelector(`#start`);
+const pauseButton = document.querySelector(`#pause`);
 
-        this.startButton.addEventListener(`click`,this.start);
+const timer = new Timer(durationInput, startButton, pauseButton, {
+    onStart() {
+        console.log(`Timer started`);
+    },
+    onTick() {
+        console.log(`TIMER JUST TICKED DOWN`);
+    },
+    onComplete() {
+        console.log(`TIMER IS COMPLETED`);
     }
-    start(){
-        console.log(`Time to start the timer!`);
-    }
-}
-
-const durationInput=document.querySelector(`#duration`);
-const startButton=document.querySelector(`#start`);
-const pauseButton=document.querySelector(`#pause`);
-
-const timer=new Timer(durationInput,startButton,pauseButton);
+});
